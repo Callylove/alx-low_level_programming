b64655c20c0acaa27789b59994b164dddf2d7f34
Callylove <nwekechidimma88@gmail.com>
2022-06-30 13:34:51 -0700

    Standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <unistd.h>
 /**
- * main - main block
- * Reurn: Always 1(Success)
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar,2015-10-19\n");
-	write(2, quote, 59);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar,2015-10-19\n");
+	write(2, str1, 59);
 	return(1);
 }
