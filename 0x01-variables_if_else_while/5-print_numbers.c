b3e2028f89bd81380fe6b2b59f28ac5ed51b7956
Callylove <nwekechidimma88@gmail.com>
2022-07-01 15:40:09 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - assign a random number and print its last digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d\n", n)
+	
+	return (0);
+}
