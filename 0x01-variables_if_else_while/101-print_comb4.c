0076cc292df14db56dff5ea425e1a5d430a6c0a7
Callylove <nwekechidimma88@gmail.com>
2022-07-02 04:34:35 -0700

    101-comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,8 +17,8 @@ int main(void)
 			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
 			{
 				putchar((digit1 % 10) + '0');
-			        putchar((digit2 % 10) + '0');
-			        putchar((digit3 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
