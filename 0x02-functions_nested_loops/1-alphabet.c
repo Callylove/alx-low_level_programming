8bc4b7cbeeb38d0687a2475cf3a102a580b54a4e
Callylove <nwekechidimma88@gmail.com>
2022-07-05 09:57:49 -0700

    lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,34 +1,17 @@
 #include "main.h"
 
 /**
- * main - prints all alphabets in lowercase using ASCII code
  * print_alphabet - calling in the function using prototype
- *
- * Return: Always 0
- */
-
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-/**
- * print_alphabet - check description
  * Description - its prints the alphabet in lowercase
  * followed by a newline
  *
- * Return: Nothing
+ * Return:Alway 0 (Success)
  */
 void print_alphabet(void)
 {
 	char letter;
 
-	int putchar(int c);
-
-	for (letter = 97; letter <= 122; letter++)
-		putchar(letter);
-	putchar('\n');
-	return (0);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
 }
