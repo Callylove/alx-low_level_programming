12080135becd84b3488a27de7ad442a2394ea42c
Callylove <nwekechidimma88@gmail.com>
2022-07-05 10:36:41 -0700

    Lowercase alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,14 @@
  *
  * Return:Alway 0 (Success)
  */
+
 void print_alphabet(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
+	{
 		_putchar(letter);
+	}
 	_putchar('\n');
 }
