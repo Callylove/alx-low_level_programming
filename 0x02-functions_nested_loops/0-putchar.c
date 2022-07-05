c3a3dcc9bc68de8de8bd0b5941e1006f9b56d0c4
Callylove <nwekechidimma88@gmail.com>
2022-07-05 05:59:29 -0700

    ASCII CODE

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	int putchar(int c); 
-
+	int putchar(int c);
 	putchar(95);
 	putchar(112);
 	putchar(117);
 	putchar(116);
 	putchar(99);
 	putchar(104);
+	putchar(97);
 	putchar(114);
 	putchar('\n');
 
