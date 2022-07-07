caeca27b899ed348b81dfc29eeb9f8c1589f49b2
Callylove <nwekechidimma88@gmail.com>
2022-07-07 15:18:11 -0700

    prints triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -19,12 +19,15 @@ void print_triangle(int size)
 	{
 		for (row = 1; row <= size; row++)
 		{
-			_putchar(' ');
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		for (hashes = 1; hashes <= row; hashes++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
 	}
 }
