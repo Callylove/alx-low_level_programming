e6a89eb9b39c746245f74f0f74fd7f3c301cc77a
Callylove <nwekechidimma88@gmail.com>
2022-07-14 15:35:20 -0700

    Prints a number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints any integer with putchar
+ * @n: number to prints
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int x;
+
+	if (n < 0)
+	{
+		_putchar('_');
+		n *= -1;
+	}
+
+	x = n;
+
+	if (x / 10)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
+}
