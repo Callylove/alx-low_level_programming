a63d6bb49a598c200a41e8b4df1c02f8603adc48
Callylove <nwekechidimma88@gmail.com>
2022-07-20 15:57:51 -0700

    Recursion 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "maiin.h"
+/**
+ * find_multipliers - find multiples of n
+ * @n: base number
+ * @i: iterator number
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+find_multipliers(int n, int i)
+{
+	if (i == n)
+		return (1);
+
+	if (n % i == 0)
+		return (0);
+
+	else
+		return (find_multipliers(n, i + 1));
+}
+/**
+ * is_prime_number - checks if n is prime
+ * @n: base number
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else
+		return (find_multipliers(n, 2));
+}
