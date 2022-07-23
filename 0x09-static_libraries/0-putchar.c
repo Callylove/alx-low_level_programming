1eec8d5024af88e7559ae48060e19ec1686d3508
Callylove <nwekechidimma88@gmail.com>
2022-07-23 09:02:06 -0700

    static library

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar using ASCII code
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int putchar(int c);
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - printing the absolute value of a number
+ * @n: the number to be checked
+ *
+ * Return: The absolute value of the number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
