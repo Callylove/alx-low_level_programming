09482344edaf610e9a160115c5f426457ae6c071
Callylove <nwekechidimma88@gmail.com>
2022-07-27 11:38:12 -0700

    malloc 5

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -40,34 +40,34 @@ char **strtow(char *str)
 		return (NULL);
 	q = (char **)malloc(n * sizeof(char *));
 	if (q == NULL)
-		return (NULL);
+	return (NULL);
 		q[n - 1] = NULL;
 		i = 0;
-		while (str[i])
+	while (str[i])
+	{
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
 		{
-			if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+		for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
+			;
+			j++;
+			q[qa] = (char *)malloc(j * sizeof(char));
+			j--;
+			if (q[qa] == NULL)
 			{
-				for (j = 1; str[i + j] != ' ' && str[i + j]; j++)
-					;
-					j++;
-					q[qa] == (char *)malloc(j * sizeof(char));
-					j--;
-					if (q[qa] == NULL)
-					{
-						for (k = 0; k < qa; k++)
-							free(q[k]);
-						free(q[n - 1]);
-						free(q);
-						return (NULL);
-					}
-					for (l = 0; l < j; l++)
-						q[qa][l] = str[i + l];
-					q[qa][l] = '\0';
-						qa++;
-						i += j;
+				for (k = 0; k < qa; k++)
+					free(q[k]);
+				free(q[n - 1]);
+					free(q);
+				return (NULL);
 			}
-			else
-				i++;
+			for (l = 0; l < j; l++)
+				q[qa][l] = str[i + j];
+			q[qa][l] = '\0';
+				qa++;
+				i += j;
 		}
-		return (q);
+		else
+			i++;
+	}
+	return (q);
 }
