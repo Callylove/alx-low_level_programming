28665b8e284a48298eeb52091c4b8dcbc663512f
Callylove <nwekechidimma88@gmail.com>
2022-08-03 14:59:01 -0700

    simple calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -15,7 +15,7 @@ int (*get_op_func(char *s))(int, int)
 	{ "+", op_add },
 	{ "-", op_sub },
 	{ "*", op_mul },
-	{ "/". op_div },
+	{ "/", op_div },
 	{ "%", op_mod },
 	{ NULL, NULL }
 	};
@@ -23,8 +23,8 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op == 0)
-				return (ops[i].f);
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
 
 		i++;
 	}
